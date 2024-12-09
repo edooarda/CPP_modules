34db543cb2ff842161edbd77d0bc972f6e7c9730
dudamakesthings <dudamakesthings@gmail.com>
2024-12-09 09:34:15 -0300

    add interaction for wich caracter

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,16 +6,16 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/27 16:03:35 by edribeir      #+#    #+#                 */
-/*   Updated: 2024/11/27 16:55:09 by edribeir      ########   odam.nl         */
+/*   Updated: 2024/12/09 09:30:42 by edooarda      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-// #include <>
 
 int main (int argc, char **argv)
 {
 	int i;
+	int j;
 
 	i = 1;
 	if (argc < 2)
@@ -26,8 +26,12 @@ int main (int argc, char **argv)
 	{
 		while(i < argc)
 		{
-			//UPPERCASE EVERYTHING AND PRINT
-			std::cout << argv[i];
+			j = 0;
+			while(argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
 			i++;
 		}
 	}
