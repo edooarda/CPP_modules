984661c19a83aeacc58be8b683254f2e339d88c9
dudamakesthings <dudamakesthings@gmail.com>
2024-12-26 12:08:19 -0300

    add WIP mod 01/ex00

---
+++ 01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Zombie.hpp                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edooarda <edooarda@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/12/26 11:37:46 by edooarda      #+#    #+#                 */
+/*   Updated: 2024/12/26 12:05:19 by edooarda      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie{
+
+	private:
+		std::string	name;
+	public:
+	Zombie(std::string name);
+	~Zombie( void );
+	void announce( void );
+};
+
+#endif
