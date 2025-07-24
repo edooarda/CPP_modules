64026a81795ae2fe0576017f9996c460f9784ac3
edooarda <edooarda@gmail.com>
2025-07-24 16:21:33 +0200

    add ex01 to cpp07

---
+++ 07/ex01/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T, typename T1, typename F>
+void iter(T* array, T1 len, F* func)
+{
+    for (T1 i = 0; i < len; ++i)
+    {
+        func(array[i]);
+    }
+}
+
+#endif
---
+++ 07/ex01/main.cpp
@@ -0,0 +1,40 @@
+#include "iter.hpp"
+
+template <typename T>
+void print(const T& value)
+{
+    std::cout << "Element: "<< value << std::endl;
+}
+
+template <typename T>
+void increase(T& elem)
+{
+    ++elem;
+}
+
+int main()
+{
+    std::cout << "String" << std::endl;
+    std::string arr_str[] = {"Bulba", "Sauro", "Pika", "Chu"};
+    ::iter(arr_str, (sizeof(arr_str)/ sizeof(arr_str[0])), print<std::string>);
+
+    std::cout << "\nInt" << std::endl;
+    int arr_int[] = {1, 2, 3 };
+    std::cout << "Before increase" << std::endl;
+    ::iter(arr_int, (sizeof(arr_int)/ sizeof(arr_int[0])), print<int>);
+    ::iter(arr_int, (sizeof(arr_int)/ sizeof(arr_int[0])), increase<int>);
+    std::cout << "After increase" << std::endl;
+    ::iter(arr_int, (sizeof(arr_int)/ sizeof(arr_int[0])), print<int>);
+
+    std::cout << "\nCharm!" << std::endl;
+    char arr_char[] = { 'H', 'E', 'L', 'P'};
+    ::iter(arr_char, (sizeof(arr_char)/ sizeof(arr_char[0])), print<char>);
+
+    std::cout << "\nFloat" << std::endl;
+    float arr_float[] = { 1.5f, 8.6f, 7.7f, 42.f};
+    std::cout << "Before increase" << std::endl;
+    ::iter(arr_float, (sizeof(arr_float)/ sizeof(arr_float[0])), print<float>);
+    ::iter(arr_float, (sizeof(arr_float)/ sizeof(arr_float[0])), increase<float>);
+    std::cout << "After increase" << std::endl;
+    ::iter(arr_float, (sizeof(arr_float)/ sizeof(arr_float[0])), print<float>);
+}
